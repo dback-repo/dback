7e9d53e7f9b002cd26aa0da919ec1cf6bc71d6c8
alexander.eliseev <alexander.eliseev@graph.uk>
2020-02-04 20:47:42 +0500

    rename test containers

--- scripts/testing/test1-save-two-containers/test.cmd
+++ scripts/testing/test1-save-two-containers/test.cmd
@@ -1,14 +1,18 @@
 cd %~dp0
 
-docker rm -f dback-test1 dback-test2
+docker rm -f dback-test1.1 dback-test1.2
+docker volume rm dback-test1.1
+
+docker volume create dback-test1.1
 
 ::infinity sleeping container
-docker run -d --rm --name dback-test1 ^
+docker run -d --rm --name dback-test1.1 ^
+	-v %CD%\data\mount-dir:/mount ^
 	alpine:3.9.5 ^
 	tail -f /dev/null 
 
 ::infinity sleeping container
-docker run -d --rm --name dback-test2 ^
-	-v %CD%\shared-test-data\mount-dir:/mount ^
+docker run -d --rm --name dback-test1.2 ^
+	-v %CD%\data\mount-dir:/mount ^
 	alpine:3.9.5 ^
 	tail -f /dev/null ::infinity sleeping container
