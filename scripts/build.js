b5fae01cd65e37042a2bbc92c4a1f2395437dfd0
alexander.eliseev <alexander.eliseev@graph.uk>
2020-02-05 23:44:49 +0500

    scratch!

--- scripts/build.js
+++ scripts/build.js
@@ -4,9 +4,10 @@ const r = process.env.REPO
 
 
 process.env.GOOS = 'linux'
+process.env.CGO_ENABLED = '0'
 
 //cmd('docker run -t --rm -v //var/run/docker.sock:/var/run/docker.sock dback dback',{stdio: 'inherit'})
 
-cmd('go build', {cwd: r+'/go-app/src/dback'});
+cmd('go build -a -installsuffix cgo', {cwd: r+'/go-app/src/dback'});
 mv(r+'/go-app/src/dback/dback', r+'/docker/dback');
 cmd('docker build -t dback .', {cwd: r+'/docker'});
