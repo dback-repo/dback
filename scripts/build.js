7cc6ee64f4137707657a9896b9f37f1218d2e5e4
alexander.eliseev <alexander.eliseev@graph.uk>
2020-02-06 20:53:58 +0500

    remove over optimization

--- scripts/build.js
+++ scripts/build.js
@@ -8,7 +8,8 @@ process.env.CGO_ENABLED = '0'
 
 //cmd('docker run -t --rm -v //var/run/docker.sock:/var/run/docker.sock dback dback',{stdio: 'inherit'})
 
-cmd('go build -a -installsuffix cgo -ldflags="-s -w"', {cwd: r+'/go-app/src/dback'});
-cmd('upx --brute dback', {cwd: r+'/go-app/src/dback'});
+//cmd('go build -a -installsuffix cgo -ldflags="-s -w"', {cwd: r+'/go-app/src/dback'});
+//cmd('upx --brute dback', {cwd: r+'/go-app/src/dback'});
+cmd('go build -a -installsuffix cgo', {cwd: r+'/go-app/src/dback'});
 mv(r+'/go-app/src/dback/dback', r+'/docker/dback');
 cmd('docker build -t dback .', {cwd: r+'/docker'});
