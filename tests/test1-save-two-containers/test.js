9c6b2f67ee98e21a97a5737b5ce8d8775e20fb0d
alexander.eliseev <alexander.eliseev@graph.uk>
2020-02-06 21:16:42 +0500

    remove --rm arg from test containers

--- tests/test1-save-two-containers/test.js
+++ tests/test1-save-two-containers/test.js
@@ -6,7 +6,6 @@ try {cmd('docker rm -f dback-test1.1 dback-test1.2')}catch{}
 try {cmd('docker volume rm dback-test1.1')}catch{}
 cmd('docker volume create dback-test1.1')
 
-cmd('docker run -d --rm --name dback-test1.1 -v %CD%\\data\\mount-dir:/mount docker:19.03.5 tail -f /dev/null')
-cmd('docker run -d --rm --name dback-test1.2 -v %CD%\\data\\mount-dir:/mount docker:19.03.5 tail -f /dev/null')
+cmd('docker run -d --name dback-test1.1 -v %CD%\\data\\mount-dir:/mount docker:19.03.5 tail -f /dev/null')
+cmd('docker run -d --name dback-test1.2 -v %CD%\\data\\mount-dir:/mount docker:19.03.5 tail -f /dev/null')
 cmd('docker run -t --rm -v //var/run/docker.sock:/var/run/docker.sock dback dback',{stdio: 'inherit'})
-
