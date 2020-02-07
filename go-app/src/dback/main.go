4c47c216a46d55bc947974b92e697f6fb902bc32
alexander.eliseev <alexander.eliseev@graph.uk>
2020-02-07 20:33:16 +0500

    backup files instead of tarballs, backup only running containers, start containers after backup

--- go-app/src/dback/main.go
+++ go-app/src/dback/main.go
@@ -2,61 +2,11 @@ package main
 
 import (
 	"context"
-	"io"
-
-	//	"log"
-	"os"
 	"sync"
-
-	"time"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 )
-
-func check(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
-func backupMount(c types.Container, m types.MountPoint, wg *sync.WaitGroup) {
-	defer wg.Done()
-	cli, err := client.NewEnvClient()
-	check(err)
-	defer cli.Close()
-
-	check(os.MkdirAll(`/backup/`+c.Names[0]+`/`+m.Destination, 0664))
-
-	reader, _, err := cli.CopyFromContainer(context.Background(), c.ID, m.Destination)
-	check(err)
-
-	outFile, err := os.Create(`/backup/` + c.Names[0] + `/` + m.Destination + `/mount-data.tar`)
-	check(err)
-	defer outFile.Close()
-	_, err = io.Copy(outFile, reader)
-	check(err)
-}
-
-func backupContainer(c types.Container, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	cli, err := client.NewEnvClient()
-	check(err)
-	defer cli.Close()
-
-	timeout := time.Minute
-	check(cli.ContainerStop(context.Background(), c.ID, &timeout))
-
-	var wgMount sync.WaitGroup
-	wgMount.Add(len(c.Mounts))
-
-	for _, curMount := range c.Mounts {
-		go backupMount(c, curMount, &wgMount)
-	}
-
-	wgMount.Wait()
-}
 
 func main() {
 	cli, err := client.NewEnvClient()
