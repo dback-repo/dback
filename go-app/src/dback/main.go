12bbbc881e30d2ffaff87dd7aa0a2acd0bf340e4
alexander.eliseev <alexander.eliseev@graph.uk>
2020-02-07 19:49:47 +0500

    Backup with dyn names (container+destination)

--- go-app/src/dback/main.go
+++ go-app/src/dback/main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"context"
 	"io"
-	"log"
+
+	//	"log"
 	"os"
 	"sync"
 
@@ -19,33 +20,48 @@ func check(err error) {
 	}
 }
 
-func backupContainer(c types.Container, wg *sync.WaitGroup) {
+func backupMount(c types.Container, m types.MountPoint, wg *sync.WaitGroup) {
 	defer wg.Done()
-	log.Println(c.Mounts)
-	log.Println(c.State)
-	log.Println(c.Status)
 	cli, err := client.NewEnvClient()
 	check(err)
+	defer cli.Close()
 
-	timeout := time.Minute
-	check(cli.ContainerStop(context.Background(), c.ID, &timeout))
-	log.Println(c.State)
-	log.Println(c.Status)
+	check(os.MkdirAll(`/backup/`+c.Names[0]+`/`+m.Destination, 0664))
 
-	reader, _, err := cli.CopyFromContainer(context.Background(), c.ID, `/mount`)
+	reader, _, err := cli.CopyFromContainer(context.Background(), c.ID, m.Destination)
 	check(err)
 
-	outFile, err := os.Create(`/backup/tar.tar`)
+	outFile, err := os.Create(`/backup/` + c.Names[0] + `/` + m.Destination + `/mount-data.tar`)
 	check(err)
 	defer outFile.Close()
 	_, err = io.Copy(outFile, reader)
-	//reader.
-	//types.ContainerPathStat
+	check(err)
+}
+
+func backupContainer(c types.Container, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	cli, err := client.NewEnvClient()
+	check(err)
+	defer cli.Close()
+
+	timeout := time.Minute
+	check(cli.ContainerStop(context.Background(), c.ID, &timeout))
+
+	var wgMount sync.WaitGroup
+	wgMount.Add(len(c.Mounts))
+
+	for _, curMount := range c.Mounts {
+		go backupMount(c, curMount, &wgMount)
+	}
+
+	wgMount.Wait()
 }
 
 func main() {
 	cli, err := client.NewEnvClient()
 	check(err)
+	defer cli.Close()
 
 	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
 	check(err)
