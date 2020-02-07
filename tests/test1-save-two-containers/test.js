5ff25ff3ecba4d93e8d8e15e55f68999eb470d59
alexander.eliseev <alexander.eliseev@graph.uk>
2020-02-07 21:34:07 +0500

    format blocks as methods

--- tests/test1-save-two-containers/test.js
+++ tests/test1-save-two-containers/test.js
@@ -11,18 +11,33 @@ const createFolder = function(dir) {
 	}
 };
 
+//one does not simply copy file to the volume!
+//we must mount the volume to temporary container
+const moveFileToDockerVolume = function(file, volume){
+	cmd('docker run --rm -v '+Path.resolve(file)+':/'+Path.basename(file)+' -v '+volume+':/dest busybox cp -r /'+Path.basename(file)+' /dest')
+}
+
+
 //move an old tmp folder to tmp-old
 //because of deleting is extrimely slow for multiple files
-createFolder('tmp-old')
-fs.renameSync('tmp', 'tmp-old/'+Math.floor(new Date() / 1000))
-createFolder('tmp')
+const clearTmp = function(){
+	createFolder('tmp-old')
+	fs.renameSync('tmp', 'tmp-old/'+Math.floor(new Date() / 1000))
+	createFolder('tmp')
+}
 
-try {cmd('docker rm -f dback-test-1.1 dback-test-1.2')}catch{}
-try {cmd('docker volume rm dback-test-1.1')}catch{}
-cmd('docker volume create dback-test-1.1')
 
-cmd('docker run -d --name dback-test-1.1 -v %CD%\\data\\mount-dir:/mount nginx:1.17.8-alpine')
-cmd('docker run -d --name dback-test-1.2 -v %CD%\\data\\mount-dir:/mount nginx:1.17.8-alpine')
+
+clearTmp()
+
+try {cmd('docker rm -f dback-test-1.1 dback-test-1.2 dback-test-1.3')}catch{}
+try {cmd('docker volume rm dback-test-1.2-volume')}catch{}
+cmd('docker volume create dback-test-1.2-volume')
+moveFileToDockerVolume('data/file1.txt','dback-test-1.2-volume')
+
+cmd('docker run -d --name dback-test-1.1 -v %CD%\\data\\mount-dir:/mount-dir nginx:1.17.8-alpine')
+cmd('docker run -d --name dback-test-1.2 -v %CD%\\data\\mount-dir:/mount-dir -v dback-test-1.2-volume:/mount-vol nginx:1.17.8-alpine')
+cmd('docker run -d --name dback-test-1.3 nginx:1.17.8-alpine')
 cmd('docker run -t --rm -v //var/run/docker.sock:/var/run/docker.sock -v %CD%\\tmp:/backup dback dback',{stdio: 'inherit'})
 
 //docker run -t --rm -v //var/run/docker.sock:/var/run/docker.sock -v %CD%\tests\test1-save-two-containers\tmp:/backup dback dback
