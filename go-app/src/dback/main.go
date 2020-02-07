fbfeab5ebccbdeedac25f185e3fcd4030ca09431
alexander.eliseev <alexander.eliseev@graph.uk>
2020-02-07 19:16:58 +0500

    stop containers and get hardcoded tar

--- go-app/src/dback/main.go
+++ go-app/src/dback/main.go
@@ -2,29 +2,60 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"io"
 	"log"
+	"os"
+	"sync"
+
+	"time"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 )
 
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func backupContainer(c types.Container, wg *sync.WaitGroup) {
+	defer wg.Done()
+	log.Println(c.Mounts)
+	log.Println(c.State)
+	log.Println(c.Status)
+	cli, err := client.NewEnvClient()
+	check(err)
+
+	timeout := time.Minute
+	check(cli.ContainerStop(context.Background(), c.ID, &timeout))
+	log.Println(c.State)
+	log.Println(c.Status)
+
+	reader, _, err := cli.CopyFromContainer(context.Background(), c.ID, `/mount`)
+	check(err)
+
+	outFile, err := os.Create(`/backup/tar.tar`)
+	check(err)
+	defer outFile.Close()
+	_, err = io.Copy(outFile, reader)
+	//reader.
+	//types.ContainerPathStat
+}
+
 func main() {
 	cli, err := client.NewEnvClient()
-	if err != nil {
-		panic(err)
-	}
+	check(err)
 
 	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
-	if err != nil {
-		panic(err)
-	}
+	check(err)
 
-	for _, container := range containers {
-		fmt.Println(container.ID)
-	}
+	var wg sync.WaitGroup
+	wg.Add(len(containers))
 
 	for _, curContainer := range containers {
-		log.Println(curContainer.Mounts)
+		go backupContainer(curContainer, &wg)
 	}
+
+	wg.Wait()
 }
