dbf8fd910b71c3cf0366df699c6f42ddb4fad5cd
alexander.eliseev <alexander.eliseev@graph.uk>
2020-02-08 16:14:53 +0500

    Update greetings, delete binary after container built

--- start-ide.cmd
+++ start-ide.cmd
@@ -9,7 +9,8 @@ cd %~dp0
 call npm i
 
 echo ***************************************************
-echo Type "iter" for build and test, change something, and run tests again. Repeat if required)
+echo Type "npm run iter" for perform build and test
+echo Change something in IDE, check the app can be compiled (ctrl+b), and run iter again. Repeat if required)
 echo Find out all commands in "scripts" directory
 echo ***************************************************
 
