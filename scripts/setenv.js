a9c722ce60948526dfdba0fba758005ab3889df8
alexander.eliseev <alexander.eliseev@graph.uk>
2020-02-11 23:02:33 +0500

    detach works

---
+++ scripts/setenv.js
@@ -0,0 +1,64 @@
+// const mv = require('fs').renameSync;
+// const cmd = require('child_process').execSync;
+
+// const r = process.env.REPO
+
+// console.log('setenv');
+// console.log(process.env.REPO);
+// //sdf
+// cmd('cmd.exe /c ping ya.ru')
+// process.env.REPO = 'testValue'
+// console.log(process.env.REPO);
+
+//cmd('node test.js', {cwd: r+'/tests/test1-save-two-containers',stdio: 'inherit'});
+
+// On Windows Only...
+// const { spawn } = require('child_process')
+// const bat = spawn('cmd.exe', ['/c', 'start cmd'])
+
+// bat.stdout.on('data', (data) => {
+//   console.log(data.toString());
+// });
+
+// bat.stderr.on('data', (data) => {
+//   console.error(data.toString());
+// });
+
+// bat.on('exit', (code) => {
+//   console.log(`Child exited with code ${code}`);
+// });
+
+// const spawn = require('child_process').spawn;
+// console.log(process.argv[0])
+// const child = spawn('cmd', ['/c start cmd'], {
+//   detached: true,
+//   stdio: ['ignore']
+// });
+
+// child.unref();
+
+
+
+
+
+
+// const child_process = require('child_process');
+// var p = child_process.fork('cmd.exe /c start cmd')
+// p.disconnect(); 
+// p.unref();
+
+
+
+
+
+
+
+
+var fs = require('fs');
+var out = fs.openSync('./out.log', 'a');
+var err = fs.openSync('./out.log', 'a');
+
+var cp = require('child_process');
+var MANFILE='setenv.js';
+var child = cp.spawn('cmd', ['/c start cmd'], { detached: true, stdio: [ 'ignore', out, err ] });
+child.unref();
--- start-ide.cmd
+++ start-ide.cmd
@@ -1,19 +1,19 @@
-@echo off
-cd %~dp0
-call scripts\setenv
-cd %~dp0
-call scripts\check-commands || exit /b
-cd %~dp0
+rem @echo off
+rem cd %~dp0
+rem call scripts\setenv
+rem cd %~dp0
+rem call scripts\check-commands || exit /b
+rem cd %~dp0
 
 
-call npm i
+rem call npm i
 
-echo ***************************************************
-echo Type "npm run iter" for perform build and test
-echo Change something in IDE, check the app can be compiled (ctrl+b), and run iter again. Repeat if required)
-echo Find out all commands in "scripts" directory
-echo ***************************************************
+rem echo ***************************************************
+rem echo Type "npm run iter" for perform build and test
+rem echo Change something in IDE, check the app can be compiled (ctrl+b), and run iter again. Repeat if required)
+rem echo Find out all commands in "scripts" directory
+rem echo ***************************************************
 
-start liteide.exe %REPO%\go-app\src\dback\main.go
+rem start liteide.exe %REPO%\go-app\src\dback\main.go
 
 cmd
