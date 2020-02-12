f71fd40663884a1a890c586964173bba4887b8e3
alexander.eliseev <alexander.eliseev@graph.uk>
2020-02-12 11:22:38 +0500

    remove old scripts

--- scripts_old/test.js
+++
@@ -1,5 +0,0 @@
-const mv = require('fs').renameSync;
-const cmd = require('child_process').execSync;
-const r = process.env.REPO
-
-cmd('node test.js', {cwd: r+'/tests/test1-save-two-containers',stdio: 'inherit'});
