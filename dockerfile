bb68e37f801c28725b062f30c554c236860abf74
alexander.eliseev <alexander.eliseev@graph.uk>
2020-02-18 22:43:46 +0500

    iterprod works

--- dockerfile
+++ dockerfile
@@ -1,9 +1,10 @@
 FROM golang:1.13.7-alpine3.11 as build
 RUN apk update && apk add build-base upx 
 COPY go-app/src /go/src
+ENV CGO_ENABLED=0
 RUN cd /go/src/dback && go build -a -installsuffix cgo -ldflags="-s -w" && upx --brute dback
 
-FROM scratch as unpacked
+FROM scratch as prod
 COPY --from=build /go/src/dback/dback /bin/dback
 
 ENV DOCKER_API_VERSION 1.37
--- scripts/build.js
+++ scripts/build.js
@@ -12,16 +12,3 @@ default:
 	console.log(process.argv)
 	throw ('Unknown build profile')
 }
-
-
-
-//process.env.GOOS = 'linux'
-//process.env.CGO_ENABLED = '0'
-
-// cmd('go build -a -installsuffix cgo -ldflags="-s -w"', {cwd: r+'/go-app/src/dback'})
-// cmd('upx --brute dback', {cwd: r+'/go-app/src/dback'})
-
-//t.cmd('go build -a -installsuffix cgo', {cwd: r+'/go-app/src/dback'})
-//t.mv(r+'/go-app/src/dback/dback', r+'/docker/dback')
-//t.cmd('docker build -t dback .', {cwd: r+'/docker'})
-//t.del(r+'/docker/dback')
