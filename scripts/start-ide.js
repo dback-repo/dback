9a0a6149ae519dbad116663865898e4da05cb954
alexander.eliseev <alexander.eliseev@graph.uk>
2020-02-19 18:55:42 +0500

    fix typos

--- scripts/start-ide.js
+++ scripts/start-ide.js
@@ -3,5 +3,5 @@ const t = new(require('./tools/tools.js'))
 t.checkCmdAvailable('docker ps')
 t.cmd('npm i')
 
-t.startCmdDetached('cmd',['/c start cmd /k echo Hi! Run \'npm run iter\' for build and test locally. Update something, and repeat.'])
+t.startCmdDetached('cmd',['/c start cmd /k echo Hi! Run \'npm run iter\' for build and test locally. Update something in IDE, check it correct (ctrl+b), and run iter again.'])
 t.startCmdDetached('liteide', [process.env['REPO']+'/go-app/src/dback/main.go'])
