9315d84d8041c0326d5b630457c6e5c77c91da83
alexander.eliseev <alexander.eliseev@graph.uk>
2020-02-19 21:52:13 +0500

    Fix current directory in test

--- tests/test1-save-two-containers/test.js
+++ tests/test1-save-two-containers/test.js
@@ -2,6 +2,7 @@ const mv = require('fs').renameSync
 var fs  = require('fs');
 const cmd = require('child_process').execSync
 const r = process.env.REPO
+const cd = process.cwd()
 
 const Path = require('path');
 
@@ -38,12 +39,12 @@ clearTmp()
 try {cmd('docker rm -f dback-test-1.1 dback-test-1.2 dback-test-1.3 dback-test-1.4')}catch{}
 initVolumeWithFile('dback-test-1.2-volume','data/file1.txt')
 initVolumeWithFile('dback-test-1.4-volume','data/file1.txt')
-cmd('docker run -d --name dback-test-1.1 -v %CD%\\data\\mount-dir:/mount-dir nginx:1.17.8-alpine')
-cmd('docker run -d --name dback-test-1.2 -v %CD%\\data\\mount-dir:/mount-dir -v dback-test-1.2-volume:/mount-vol nginx:1.17.8-alpine')
+cmd('docker run -d --name dback-test-1.1 -v '+cd+'\\data\\mount-dir:/mount-dir nginx:1.17.8-alpine')
+cmd('docker run -d --name dback-test-1.2 -v '+cd+'\\data\\mount-dir:/mount-dir -v dback-test-1.2-volume:/mount-vol nginx:1.17.8-alpine')
 cmd('docker run -d --name dback-test-1.3 nginx:1.17.8-alpine')
 cmd('docker run --rm -d --name dback-test-1.4 -v dback-test-1.4-volume:/mount-vol nginx:1.17.8-alpine')
 
-cmd('docker run -t --rm -v //var/run/docker.sock:/var/run/docker.sock -v %CD%\\tmp:/backup dback backup',{stdio: 'inherit'})
-// cmd('docker run -t --rm -v //var/run/docker.sock:/var/run/docker.sock -v %CD%\\tmp:/backup dback restore',{stdio: 'inherit'})
+cmd('docker run -t --rm -v //var/run/docker.sock:/var/run/docker.sock -v '+cd+'\\tmp:/backup dback backup',{stdio: 'inherit'})
+// cmd('docker run -t --rm -v //var/run/docker.sock:/var/run/docker.sock -v '+cd+'\\tmp:/backup dback restore',{stdio: 'inherit'})
 
 //docker run --rm -v //var/run/docker.sock:/var/run/docker.sock -v %REPO%\tests\test1-save-two-containers\tmp:/backup dback restore
