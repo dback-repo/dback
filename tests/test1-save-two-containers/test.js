5bc36a2e3b9e6fbe1f874133ccd6ae34936f2c16
alexander.eliseev <alexander.eliseev@graph.uk>
2020-02-22 00:01:27 +0500

    Check app output in the test

--- tests/test1-save-two-containers/test.js
+++ tests/test1-save-two-containers/test.js
@@ -1,6 +1,7 @@
 const mv = require('fs').renameSync
 var fs  = require('fs');
 const cmd = require('child_process').execSync
+const spw = require('child_process').spawnSync
 const r = process.env.REPO
 const cd = process.cwd()
 
@@ -18,7 +19,6 @@ const moveFileToDockerVolume = function(file, volume){
 	cmd('docker run --rm -v '+Path.resolve(file)+':/'+Path.basename(file)+' -v '+volume+':/dest busybox cp -r /'+Path.basename(file)+' /dest')
 }
 
-
 //move an old tmp folder to tmp-old
 //because of deleting is extrimely slow for multiple files
 const clearTmp = function(){
@@ -34,17 +34,34 @@ const initVolumeWithFile = function(volumeName, filename){
 	moveFileToDockerVolume(filename, volumeName)
 }
 
+const checkSub = function(str, substr){
+	if (!str.includes(substr)){
+		throw('"'+substr+'" is expected but not found in the string:'+"\r\n"+str)
+	}
+}
+
+const checkNoSub = function(str, substr){
+	if (str.includes(substr)){
+		throw('"'+substr+'" is expected but not found in the string:'+"\r\n"+str)
+	}
+}
+
 
 clearTmp()
-try {cmd('docker rm -f dback-test-1.1 dback-test-1.2 dback-test-1.3 dback-test-1.4')}catch{}
+try {cmd('docker rm -f dback-test-1.1 dback-test-1.2 dback-test-1.3 dback-test-1.4',{stdio: 'ignore'})}catch{}
 initVolumeWithFile('dback-test-1.2-volume','data/file1.txt')
 initVolumeWithFile('dback-test-1.4-volume','data/file1.txt')
-cmd('docker run -d --name dback-test-1.1 -v '+cd+'\\data\\mount-dir:/mount-dir nginx:1.17.8-alpine')
-cmd('docker run -d --name dback-test-1.2 -v '+cd+'\\data\\mount-dir:/mount-dir -v dback-test-1.2-volume:/mount-vol nginx:1.17.8-alpine')
+cmd('docker run -d --name dback-test-1.1 -v '+cd+'/data/mount-dir:/mount-dir nginx:1.17.8-alpine')
+cmd('docker run -d --name dback-test-1.2 -v '+cd+'/data/mount-dir:/mount-dir -v dback-test-1.2-volume:/mount-vol nginx:1.17.8-alpine')
 cmd('docker run -d --name dback-test-1.3 nginx:1.17.8-alpine')
 cmd('docker run --rm -d --name dback-test-1.4 -v dback-test-1.4-volume:/mount-vol nginx:1.17.8-alpine')
 
-cmd('docker run -t --rm -v //var/run/docker.sock:/var/run/docker.sock -v '+cd+'\\tmp:/backup dback backup',{stdio: 'inherit'})
-// cmd('docker run -t --rm -v //var/run/docker.sock:/var/run/docker.sock -v '+cd+'\\tmp:/backup dback restore',{stdio: 'inherit'})
+var out = cmd('docker run -t --rm -v //var/run/docker.sock:/var/run/docker.sock -v '+cd+'/tmp:/backup dback backup').toString()
+checkSub(out,'Backup started')
+checkSub(out,'/dback-test-1.2/mount-vol')
+checkSub(out,'/dback-test-1.1/mount-dir')
+checkSub(out,'/dback-test-1.2/mount-dir')
+checkSub(out,'Backup has finished for the mounts above')
+checkNoSub(out,'dback-test-1.4')
 
-//docker run --rm -v //var/run/docker.sock:/var/run/docker.sock -v %REPO%\tests\test1-save-two-containers\tmp:/backup dback restore
+cmd('docker rm -f dback-test-1.1 dback-test-1.2 dback-test-1.3 dback-test-1.4')
