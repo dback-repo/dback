11decd53f8a241a14e80e156bb3566d62321fc28
alexander.eliseev <alexander.eliseev@graph.uk>
2020-03-04 20:23:42 +0500

    Add test for mount excluding

--- tests/test1-save-two-containers/test.js
+++ tests/test1-save-two-containers/test.js
@@ -48,20 +48,22 @@ const checkNoSub = function(str, substr){
 
 
 clearTmp()
-try {cmd('docker rm -f dback-test-1.1 dback-test-1.2 dback-test-1.3 dback-test-1.4',{stdio: 'ignore'})}catch{}
+try {cmd('docker rm -f dback-test-1.1 dback-test-1.2 dback-test-1.3 dback-test-1.4 dback-test-1.5',{stdio: 'ignore'})}catch{}
 initVolumeWithFile('dback-test-1.2-volume','data/file1.txt')
 initVolumeWithFile('dback-test-1.4-volume','data/file1.txt')
 cmd('docker run -d --name dback-test-1.1 -v '+cd+'/data/mount-dir:/mount-dir nginx:1.17.8-alpine')
 cmd('docker run -d --name dback-test-1.2 -v '+cd+'/data/mount-dir:/mount-dir -v dback-test-1.2-volume:/mount-vol nginx:1.17.8-alpine')
 cmd('docker run -d --name dback-test-1.3 nginx:1.17.8-alpine')
 cmd('docker run --rm -d --name dback-test-1.4 -v dback-test-1.4-volume:/mount-vol nginx:1.17.8-alpine')
+cmd('docker run -d --name dback-test-1.5 -v '+cd+'/data/mount-dir:/mount-dir nginx:1.17.8-alpine')
 
-var out = cmd('docker run -t --rm -v //var/run/docker.sock:/var/run/docker.sock -v '+cd+'/tmp:/backup dback backup --exclude-mount ^/drone.*$').toString()
+var out = cmd('docker run -t --rm -v //var/run/docker.sock:/var/run/docker.sock -v '+cd+'/tmp:/backup dback backup --exclude-mount "^/(drone.*|dback-test-1.5.*)$"').toString()
 checkSub(out,'Backup started')
 checkSub(out,'make backup: /dback-test-1.2/mount-vol')
 checkSub(out,'make backup: /dback-test-1.1/mount-dir')
 checkSub(out,'make backup: /dback-test-1.2/mount-dir')
+checkSub(out,'exclude: /dback-test-1.5/mount-dir')
 checkSub(out,'Backup has finished for the mounts above')
 checkNoSub(out,'dback-test-1.4')
 
-cmd('docker rm -f dback-test-1.1 dback-test-1.2 dback-test-1.3 dback-test-1.4')
+cmd('docker rm -f dback-test-1.1 dback-test-1.2 dback-test-1.3 dback-test-1.4 dback-test-1.5')
