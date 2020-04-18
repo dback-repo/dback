3cfc4e6106cc30a592248d271f54da74dc3264c5
alexander.eliseev <alexander.eliseev@graph.uk>
2020-04-18 12:21:28 +0500

    Cut commented

--- src/cobraex/cmd/backup.go
+++ src/cobraex/cmd/backup.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2020 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -27,7 +12,18 @@ var exclude string
 var backupCmd = &cobra.Command{
 	Use:   "backup",
 	Short: "Make backup, and pass it to S3 bucket",
-	Long:  `Longer description: Make backup, and pass it to S3 bucket`,
+	Long: `Make backup, and pass it to S3 bucket
+  Create snapshot of mounts matched all the points:
+  default points:
+  - HostConfig.AutoRemove:      false
+  - HostConfig.RestartPolicy:   always
+
+Options:
+  --exclude            Exclude volume pattern
+    mounts are named as: [ContainerName]/[PathInContainer]
+    For example, mount in "mysql" container: mysql/var/mysql/data
+    Pattern is a regular expression. For example, "^/(drone.*|dback-test-1.5.*)$"
+    ignore all mounts starts with "/drone", or "/dback-test-1.5"`,
 
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("backup called")
@@ -36,21 +32,10 @@ var backupCmd = &cobra.Command{
 		//fmt.Println(`exclude`, cmd.Flag(`exclude`).Value.String())
 		fmt.Println(`emulate`, emulate)
 		fmt.Println(`exclude`, exclude)
-
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(backupCmd)
 	backupCmd.PersistentFlags().StringVarP(&exclude, "exclude", "x", ``, "Exclude containers by name, matched with RegEx")
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// backupCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// backupCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- src/cobraex/cmd/root.go
+++ src/cobraex/cmd/root.go
@@ -33,25 +33,5 @@ func Execute() {
 var emulate bool
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobraex.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	//rootCmd.Flags().BoolP("emulate", "e", false, "emulate write actions, and show list of items that will be affected")
-	//cobra.MarkFlagRequired()
-
 	rootCmd.PersistentFlags().BoolVarP(&emulate, "emulate", "e", false, "emulate an action, and show list of items will be affected")
-	//rootCmd.MarkPersistentFlagRequired(`emulate`)
-	//fmt.Println(emulate)
-	//rootCmd.ParseFlags()
-
-	//rootCmd.Flags()
-	//rootCmd.Flags().
-
-	// rootCmd.AddCommand(backupCmd)
-	// rootCmd.AddCommand(restoreCmd)
 }
