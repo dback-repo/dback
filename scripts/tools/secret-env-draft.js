e6f907670b5f13bbcdb453c379c993ab44d82732
alexander.eliseev <alexander.eliseev@graph.uk>
2020-05-30 12:55:23 +0500

    update secrets env variables draft

--- scripts/tools/secret-env-draft.js
+++ scripts/tools/secret-env-draft.js
@@ -1,7 +1,8 @@
 module.exports = class SecEnv {
 	constructor()
     {
-    	process.env['DBACK_DOCKER_LOGIN']       = ''
+        process.env['DBACK_DOCKER_LOGIN']       = ''
         process.env['DBACK_DOCKER_PASSWORD']    = ''
+        process.env['DBACK_DOCKER_REPO']    	= ''
     }
 };
