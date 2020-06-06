add86cf4b2d5511f1b3e3dc5a0f5e7c0ffa4e584
alexander.eliseev <alexander.eliseev@graph.uk>
2020-06-07 01:47:18 +0500

    select latest snapshot

--- src/dback/logic/restore.go
+++ src/dback/logic/restore.go
@@ -10,9 +10,25 @@ import (
 func getS3MountsForRestore(s3 *s3wrapper.S3Wrapper, resticw *resticwrapper.ResticWrapper,
 	dockerw *dockerwrapper.DockerWrapper, restoreParams []string) []s3wrapper.S3Mount {
 	s3Mounts := s3.GetMounts(resticw, dockerw)
-	res := s3Mounts
 
-	log.Println(`restoreParams`, restoreParams)
+	localMounts := dockerw.GetMountsOfContainers(dockerw.GetAllContainers())
+
+	res := []s3wrapper.S3Mount{}
+
+	for _, curLocalMount := range localMounts {
+		for _, curS3Mount := range s3Mounts {
+			if curS3Mount.ContainerName+curS3Mount.Dest == curLocalMount.ContainerName+curLocalMount.MountDest {
+				res = append(res, curS3Mount)
+			}
+		}
+	}
+
+	for mountIdx, curS3Mount := range res {
+		res[mountIdx].SelectedSnapshotID = curS3Mount.Snapshots[len(curS3Mount.Snapshots)-1].ID
+	}
+
+	log.Println(`localMounts:`, localMounts)
+	log.Println(`restoreParams:`, restoreParams)
 
 	return res
 }
